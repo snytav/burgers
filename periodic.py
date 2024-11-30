28ffc627740631fee1d39059e8509d404e91717c
snytav <snytav@gmail.com>
2024-12-01 00:00:14 +0200

    prepared function for periodic BC - 1st launch

--- periodic.py
+++ periodic.py
@@ -1,7 +1,7 @@
 from dolfin import *
 
 def get_periodic_BC(u0,V,on_left,on_right):
-    u = u0.vectors().get_local()
+    u = u0.vector().get_local()
     bc_left = DirichletBC(V, u[-2], on_left)
     bc_right = DirichletBC(V, u[1], on_right)
     bc = [bc_left, bc_right]
